6c4218c3a6a660447055ce7ae9c196c9b20828d2
LynnTseng <lynntseng@liontravel.com>
2019-09-03 17:14:48 +0800

    - spring-boot升版2.1.7 - 新增SignatureTest

---
+++ src/test/java/com/linebot/appengine/service/SignatureTest.java
@@ -0,0 +1,24 @@
+package com.linebot.appengine.service;
+
+import java.util.Base64;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import org.junit.Test;
+
+public class SignatureTest {
+
+  @Test
+  public void createTest() throws Exception {
+    String channelSecret = "110d29531c085354329394e2b6eefee5";
+    // Request body string
+    String httpRequestBody =
+        "{\"events\":[{\"type\":\"message\",\"replyToken\":\"ef9d86c9cbd84a7389e682fb120c8511\",\"source\":{\"userId\":\"Ua73de9ca23aa0664b9f6b99f4c55796d\",\"type\":\"user\"},\"timestamp\":1567481188463,\"message\":{\"type\":\"text\",\"id\":\"10503215608937\",\"text\":\"111\"}}],\"destination\":\"Ua73de9ca23aa0664b9f6b99f4c55796d\"}";
+    SecretKeySpec key = new SecretKeySpec(channelSecret.getBytes(), "HmacSHA256");
+    Mac mac = Mac.getInstance("HmacSHA256");
+    mac.init(key);
+    byte[] source = httpRequestBody.getBytes("UTF-8");
+    String signature = Base64.getEncoder().encodeToString(mac.doFinal(source));
+    System.out.println(signature);
+  }
+
+}
