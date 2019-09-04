0f9e7aa528a19830c4be473ca1602ab7cc9b3254
LynnTseng <lynntseng@liontravel.com>
2019-09-04 14:48:03 +0800

    衝突調整

--- src/main/java/com/linebot/appengine/service/AbstractService.java
+++ src/main/java/com/linebot/appengine/service/AbstractService.java
@@ -1,6 +1,5 @@
 package com.linebot.appengine.service;
 
-import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.linecorp.bot.client.LineMessagingClient;
 import com.linecorp.bot.spring.boot.LineBotProperties;
@@ -10,11 +9,7 @@ public class AbstractService {
   @Autowired
   protected LineBotProperties lineBotProperties;
 
+  @Autowired
   protected LineMessagingClient client;
-
-  @PostConstruct
-  private void init() {
-    client = LineMessagingClient.builder(lineBotProperties.getChannelToken()).build();
-  }
 
 }
--- src/main/java/com/linebot/appengine/service/EchoService.java
+++ src/main/java/com/linebot/appengine/service/EchoService.java
@@ -1,8 +1,13 @@
 package com.linebot.appengine.service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import com.linecorp.bot.model.PushMessage;
+import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.StickerMessageContent;
@@ -19,12 +24,6 @@ public class EchoService extends AbstractService {
   private static final Logger LOGGER = LoggerFactory.getLogger(EchoService.class);
 
   @EventMapping
-  public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-    LOGGER.info("Request Data: {}", event);
-    return new TextMessage(event.getMessage().getText());
-  }
-
-  @EventMapping
   public StickerMessage handleStickerMessageEvent(MessageEvent<StickerMessageContent> event) {
     LOGGER.info("Request Data: {}", event);
     return new StickerMessage(event.getMessage().getPackageId(), event.getMessage().getStickerId());
@@ -35,4 +34,21 @@ public class EchoService extends AbstractService {
     LOGGER.info("Request Data: {}", event);
   }
 
+  @EventMapping
+  public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws Exception {
+    if ("hi".equalsIgnoreCase(event.getMessage().getText())) {
+      // push 活動內容
+      List<String> actions = new ArrayList<>();
+      actions.add("優惠活動1:\n華納威秀電影團體券：230元/張\n剩餘 15 組");
+      actions.add("優惠活動2:\n朱銘美術館參觀票：好康價150元/張\n剩餘 8 組");
+      actions.add("優惠活動3:\n六福村中午12點以後入園：480元/張\n剩餘 19 組");
+      actions.add("優惠活動4:\n陶板屋-和風創作料理套餐券：585元/張\n剩餘 6 組");
+      actions.forEach(act -> client.pushMessage(new PushMessage(event.getSource().getUserId(),
+          Collections.singletonList(new TextMessage(act)))));
+    } else {
+      client.replyMessage(new ReplyMessage(event.getReplyToken(),
+          Collections.singletonList(new TextMessage(event.getSource().getUserId())))).get();
+    }
+
+  }
 }
