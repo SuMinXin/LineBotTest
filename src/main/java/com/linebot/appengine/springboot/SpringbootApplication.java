97b262ff2ea2cc07636950b09e8adcbfa4988598
suminxin <minxinsu@liontravel.com>
2019-09-04 10:26:15 +0800

    no message

---
+++ src/main/java/com/linebot/appengine/springboot/SpringbootApplication.java
@@ -0,0 +1,22 @@
+package com.linebot.appengine.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+@ImportResource("com.linebot.appengine.service.*")
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hi, 歡迎來到LineBot測試!!";
+	}
+
+}
