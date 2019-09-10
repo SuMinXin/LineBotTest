74f0019c99995d88a458dbb9965d13fda35f6328
LynnTseng <lynntseng@liontravel.com>
2019-09-10 11:46:30 +0800

    - 更換token - 新增richMenu資源

--- src/main/java/com/linebot/service/ActiveService.java
+++ src/main/java/com/linebot/service/ActiveService.java
@@ -10,7 +10,6 @@ import java.util.Map;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.google.api.services.sheets.v4.model.ValueRange;
@@ -19,140 +18,141 @@ import com.linebot.bean.Product;
 @Service
 public class ActiveService {
 
-	@Autowired
-	private OrderService orderService;
+  @Autowired
+  private OrderService orderService;
 
-	private Lock lock = new ReentrantLock();
+  private Lock lock = new ReentrantLock();
 
-	private GoogleSheetService sheetService = GoogleSheetService.getInstance();
+  private GoogleSheetService sheetService = GoogleSheetService.getInstance();
 
-	private static final String SHEET_ID = "1qenyxoIhzbHK-09nVxnVpDBlp3LepvQ7ALmXZKzPV7s";
+  private static final String SHEET_ID = "1qenyxoIhzbHK-09nVxnVpDBlp3LepvQ7ALmXZKzPV7s";
 
-	private static final String SHEET_NAME = "Action!";
+  private static final String SHEET_NAME = "Action!";
 
-	private static Map<String, Product> productsMap = new HashMap<>();
+  private static Map<String, Product> productsMap = new HashMap<>();
 
-	private static List<Integer> exprireTime = new ArrayList<>(Arrays.asList(0, 10, 20, 30, 40, 50));
+  private static List<Integer> exprireTime = new ArrayList<>(Arrays.asList(0, 10, 20, 30, 40, 50));
 
-	// ---------------------------- 依Message 回覆 ----------------------------
-	// HI
-	public List<String> getProductList() {
-		return getProducts().stream().map(action -> {
-			StringBuilder active = new StringBuilder();
-			active.append("活動名稱：");
-			active.append(action.getName());
-			active.append('\n');
-			active.append(getActiveDesc(action));
-			return active.toString();
-		}).collect(Collectors.toList());
-	}
+  // ---------------------------- 依Message 回覆 ----------------------------
+  // HI
+  public List<String> getProductList() {
+    return getProducts().stream().map(action -> {
+      StringBuilder active = new StringBuilder();
+      active.append("活動名稱：");
+      active.append(action.getName());
+      active.append('\n');
+      active.append(getActiveDesc(action));
+      return active.toString();
+    }).collect(Collectors.toList());
+  }
 
-	// YO
-	public List<Product> getProducts() {
-		readData();
-		return new ArrayList<>(productsMap.values());
-	}
+  // YO
+  public List<Product> getProducts() {
+    readData();
+    return new ArrayList<>(productsMap.values());
+  }
 
-	public void resetData() {
-		productsMap.clear();
-		readData();
-	}
+  public void resetData() {
+    productsMap.clear();
+    readData();
+  }
 
-	// ---------------------------- 購買商品 ----------------------------
-	public String sell(String id) throws Exception {
-		// 數量 -1
-		lock.lock();
-		String response = null;
-		try {
-			Product action = productsMap.get(id);
-			if (action == null) {
-				throw new Exception("Active Not Exist");
-			}
-			response = action.getName();
-			if (action.getAmount() > 0) {
-				action.setAmount(action.getAmount() - 1);
-				updateProduct(id);
-			} else {
-				throw new Exception("Sold Out");
-			}
-		} finally {
-			lock.unlock();
-		}
-		return response;
-	}
+  // ---------------------------- 購買商品 ----------------------------
+  public String sell(String id) throws Exception {
+    // 數量 -1
+    lock.lock();
+    String response = null;
+    try {
+      Product action = productsMap.get(id);
+      if (action == null) {
+        throw new Exception("Active Not Exist");
+      }
+      response = action.getName();
+      if (action.getAmount() > 0) {
+        action.setAmount(action.getAmount() - 1);
+        updateProduct(id);
+      } else {
+        throw new Exception("Sold Out");
+      }
+    } finally {
+      lock.unlock();
+    }
+    return response;
+  }
 
-	public void addOrder(String itemID, String userID) {
-		orderService.addPax(itemID, userID, 1, productsMap.get(itemID).getPrice());
-	}
+  public void addOrder(String itemID, String userID) {
+    orderService.addPax(itemID, userID, 1, productsMap.get(itemID).getPrice());
+  }
 
-	// 活動結算
-	public void activeFinished(String itemID) {
-		orderService.activeFinished(itemID);
-		productsMap.remove(itemID);
-	}
+  // 活動結算
+  public void activeFinished(String itemID) {
+    orderService.activeFinished(itemID);
+    productsMap.remove(itemID);
+  }
 
-	// ---------------------------- Product Function ----------------------------
-	private void readData() {
-		if (productsMap.isEmpty() || isExpire()) {
-			// https://docs.google.com/spreadsheets/d/1qenyxoIhzbHK-09nVxnVpDBlp3LepvQ7ALmXZKzPV7s/edit#gid=0
-			String range = SHEET_NAME + "A2:H";
-			List<List<Object>> response = GoogleSheetService.getInstance().readGoogleSheet(SHEET_ID, range);
-			if (!response.isEmpty()) {
-				List<Product> acts = response.stream().map(this::toProduct).collect(Collectors.toList());
-				for (Product act : acts) {
-					if (!productsMap.containsKey(act.getId())) {
-						productsMap.put(act.getId(), act);
-					}
-				}
-			}
-		}
-	}
+  // ---------------------------- Product Function ----------------------------
+  private void readData() {
+    if (productsMap.isEmpty() || isExpire()) {
+      // https://docs.google.com/spreadsheets/d/1qenyxoIhzbHK-09nVxnVpDBlp3LepvQ7ALmXZKzPV7s/edit#gid=0
+      String range = SHEET_NAME + "A2:H";
+      List<List<Object>> response =
+          GoogleSheetService.getInstance().readGoogleSheet(SHEET_ID, range);
+      if (!response.isEmpty()) {
+        List<Product> acts = response.stream().map(this::toProduct).collect(Collectors.toList());
+        for (Product act : acts) {
+          if (!productsMap.containsKey(act.getId())) {
+            productsMap.put(act.getId(), act);
+          }
+        }
+      }
+    }
+  }
 
-	private boolean isExpire() {
-		if (LocalDateTime.now().getMinute() > exprireTime.get(0)) {
-			exprireTime.add(exprireTime.get(0));
-			exprireTime.remove(0);
-			return true;
-		} else {
-			return false;
-		}
-	}
+  private boolean isExpire() {
+    if (LocalDateTime.now().getMinute() > exprireTime.get(0)) {
+      exprireTime.add(exprireTime.get(0));
+      exprireTime.remove(0);
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-	private Product toProduct(List<Object> object) {
-		Product product = new Product();
-		product.setId(object.get(0).toString());
-		product.setName(object.get(1).toString());
-		product.setDesc(object.get(2).toString());
-		product.setPrice(BigDecimal.valueOf(Integer.valueOf(object.get(3).toString())));
-		product.setUnit(object.get(4).toString());
-		product.setAmount(Integer.valueOf(object.get(5).toString()));
-		product.setImageUrl(object.get(6).toString());
-		product.setActiveUrl(object.get(7).toString());
-		return product;
-	}
+  private Product toProduct(List<Object> object) {
+    Product product = new Product();
+    product.setId(object.get(0).toString());
+    product.setName(object.get(1).toString());
+    product.setDesc(object.get(2).toString());
+    product.setPrice(BigDecimal.valueOf(Integer.valueOf(object.get(3).toString())));
+    product.setUnit(object.get(4).toString());
+    product.setAmount(Integer.valueOf(object.get(5).toString()));
+    product.setImageUrl(object.get(6).toString());
+    product.setActiveUrl(object.get(7).toString());
+    return product;
+  }
 
-	private void updateProduct(String id) {
-		Product product = productsMap.get(id);
-		ValueRange data = toValueRange(product);
-		sheetService.updateGoogleSheet(SHEET_ID, Arrays.asList(data));
-	}
+  private void updateProduct(String id) {
+    Product product = productsMap.get(id);
+    ValueRange data = toValueRange(product);
+    sheetService.updateGoogleSheet(SHEET_ID, Arrays.asList(data));
+  }
 
-	// ---------------------------- Format ----------------------------
-	public String getActiveDesc(Product product) {
-		StringBuilder description = new StringBuilder();
-		description.append(product.getDesc());
-		description.append('\n');
-		description.append(product.getPrice());
-		description.append("/");
-		description.append(product.getUnit());
-		description.append('\n');
-		description.append("剩餘數量：").append(product.getAmount());
-		return description.toString();
-	}
+  // ---------------------------- Format ----------------------------
+  public String getActiveDesc(Product product) {
+    StringBuilder description = new StringBuilder();
+    description.append(product.getDesc());
+    description.append('\n');
+    description.append(product.getPrice());
+    description.append("/");
+    description.append(product.getUnit());
+    description.append('\n');
+    description.append("剩餘數量：").append(product.getAmount());
+    return description.toString();
+  }
 
-	private ValueRange toValueRange(Product product) {
-		return new ValueRange().setRange(SHEET_NAME + "F" + (Integer.valueOf(product.getId()) + 1))
-				.setValues(Arrays.asList(Arrays.asList(product.getAmount())));
-	}
+  private ValueRange toValueRange(Product product) {
+    return new ValueRange().setRange(SHEET_NAME + "F" + (Integer.valueOf(product.getId()) + 1))
+        .setValues(Arrays.asList(Arrays.asList(product.getAmount())));
+  }
 
 }
---
+++ src/main/java/com/linebot/service/LineBotService.java
@@ -0,0 +1,179 @@
+package com.linebot.service;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.linebot.bean.Product;
+import com.linecorp.bot.model.Multicast;
+import com.linecorp.bot.model.PushMessage;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.action.Action;
+import com.linecorp.bot.model.action.PostbackAction;
+import com.linecorp.bot.model.action.URIAction;
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.PostbackEvent;
+import com.linecorp.bot.model.event.message.StickerMessageContent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.StickerMessage;
+import com.linecorp.bot.model.message.TemplateMessage;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.message.template.CarouselColumn;
+import com.linecorp.bot.model.message.template.CarouselTemplate;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+@Service
+@LineMessageHandler
+public class LineBotService extends AbstractService {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(LineBotService.class);
+
+  private static final String LOG_RQ = "Request Data: {}";
+
+  private static final String DEFAULT_URL = "https://www.liontravel.com/category/zh-tw/index";
+
+  private static final String DEFAULT_IMG_URL =
+      "https://activity.liontravel.com/Images/Activity_Loading.jpg";
+
+  @Autowired
+  private ActiveService activeService;
+
+  @EventMapping
+  public void handleDefaultMessageEvent(Event event) {
+    LOGGER.info(LOG_RQ, event);
+  }
+
+  @EventMapping
+  public StickerMessage handleStickerMessageEvent(MessageEvent<StickerMessageContent> event) {
+    LOGGER.info(LOG_RQ, event);
+    String packageId = event.getMessage().getPackageId();
+    String stickerId = event.getMessage().getStickerId();
+    if ("2".equals(packageId) && "32".equals(stickerId)) {
+      activeService.resetData();
+      return new StickerMessage("1", "109");
+    }
+
+    return new StickerMessage(packageId, stickerId);
+  }
+
+  @EventMapping
+  public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+    String text = event.getMessage().getText();
+    String userId = event.getSource().getUserId();
+    try {
+      switch (text.toUpperCase()) {
+        case "HI":
+          lineMessagingClient.pushMessage(new PushMessage(userId, activeService.getProductList()
+              .stream().map(TextMessage::new).collect(Collectors.toList())));
+          break;
+        case "YO":
+          Message message = new TemplateMessage("優惠活動來囉~~", getCarouselTemplate());
+          lineMessagingClient.pushMessage(new PushMessage(userId, message));
+          break;
+        default:
+          if (text.contains("+")) {
+            int item = Integer.parseInt(text.replace("+", ""));
+            sellItem(String.valueOf(item), event.getReplyToken(), event.getSource().getUserId());
+          } else {
+            defaultMessage(event);
+          }
+          break;
+      }
+    } catch (Exception e) {
+      LOGGER.info(LOG_RQ, event);
+      defaultMessage(event);
+    }
+  }
+
+  @EventMapping
+  public void handlePostBackEvent(PostbackEvent event)
+      throws InterruptedException, ExecutionException {
+    LOGGER.info(LOG_RQ, event);
+
+    try {
+      sellItem(event.getPostbackContent().getData(), event.getReplyToken(),
+          event.getSource().getUserId());
+    } catch (Exception e) {
+      defaultMessage(event);
+    }
+  }
+
+  private void sellItem(String itemID, String replyToken, String userID)
+      throws InterruptedException, ExecutionException {
+    String actName = "";
+    try {
+      actName = activeService.sell(itemID);
+      lineMessagingClient
+          .replyMessage(new ReplyMessage(replyToken, new TextMessage(actName + " 購買成功!"))).get();
+      activeService.addOrder(itemID, userID);
+    } catch (Exception e) {
+      if ("Active Not Exist".equalsIgnoreCase(e.getMessage())) {
+        lineMessagingClient
+            .replyMessage(new ReplyMessage(replyToken, new TextMessage("很抱歉，您選購的商品不存在唷"))).get();
+      } else {
+        lineMessagingClient.replyMessage(new ReplyMessage(replyToken,
+            new TextMessage("很抱歉，您選購的商品" + actName + " 已銷售完畢囉!\n期待您的下次選購^_^"))).get();
+      }
+    }
+  }
+
+  private CarouselTemplate getCarouselTemplate() {
+    List<CarouselColumn> columns = activeService.getProducts().stream().map(this::toCarouselColumn)
+        .collect(Collectors.toList());
+    return new CarouselTemplate(columns);
+  }
+
+  private URI getURI(String url, String def) {
+    try {
+      if (url.isBlank() || url.isEmpty()) {
+        return new URI(def);
+      }
+      return new URI(url);
+    } catch (URISyntaxException ex) {
+      LOGGER.error("URISyntaxException:{}/{}", url, def);
+      return null;
+    }
+  }
+
+  private CarouselColumn toCarouselColumn(Product product) {
+    // Carousel最多3個Actions
+    Action action1 = new URIAction("詳細內容", getURI(product.getActiveUrl(), DEFAULT_URL), null);
+    Action action2 = new PostbackAction("立即購買", product.getId(), null);
+    return new CarouselColumn(getURI(product.getImageUrl(), DEFAULT_IMG_URL), product.getName(),
+        activeService.getActiveDesc(product), Arrays.asList(action1, action2));
+  }
+
+  private void defaultMessage(Event event) {
+    try {
+      lineMessagingClient.multicast(getErrorMulticast(event.getSource().getUserId()));
+    } catch (Exception e) {
+      LOGGER.info(LOG_RQ, event);
+    }
+  }
+
+  private Multicast getErrorMulticast(String userId) {
+    Set<String> user = new HashSet<>();
+    user.add(userId);
+
+    String exMessage = "這個帳號沒有辦法對您剛才的訊息內容做出回覆。\n試試看送出 【HI】、【YO】 ，可以看到目前最新的優惠活動唷~ \n期待您下次的訊息內容！";
+    List<Message> message = new ArrayList<>();
+    // https://devdocs.line.me/files/sticker_list.pdf
+    message.add(new StickerMessage("2", "38"));
+    message.add(new TextMessage(exMessage));
+
+    return new Multicast(user, message);
+  }
+
+}
