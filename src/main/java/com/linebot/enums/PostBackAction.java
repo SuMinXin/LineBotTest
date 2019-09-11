44abda1bf473c7dcd437bb5abc0ddf1bf3de88e4
LIONMAIL\victorkung <victorkung@liontravel.com>
2019-09-11 11:32:10 +0800

    查看group的user profile

--- src/main/java/com/linebot/enums/PostBackAction.java
+++ src/main/java/com/linebot/enums/PostBackAction.java
@@ -1,12 +1,16 @@
 package com.linebot.enums;
 
 public enum PostBackAction {
-  BUY("BUY"),
-  ORDER_DETAIL("ORDER");
+  BUY("BUY"), ORDER_DETAIL("ORDER");
 
   private final String type;
 
   private PostBackAction(String type) {
     this.type = type;
   }
+
+  public String getType() {
+    return type;
+  }
+
 }
